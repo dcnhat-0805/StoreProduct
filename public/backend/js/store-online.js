7d64556556ef501a2c3ab0f23a10e0f41f133375
dcnhat-0805 <nhat605117@student.vnua.edu.vn>
2019-08-04 18:51:27 +0700

    develop: fix menu

--- public/backend/js/store-online.js
+++ public/backend/js/store-online.js
@@ -1,12 +1,12 @@
 var Common = (function ($) {
     var Common = {};
+
     Common.hasData = function ($name = 'nhat') {
         console.log($name);
     }
 
-
     return Common;
-})();
+})(jQuery);
 
 $.ajaxSetup({
     headers: {
@@ -24,7 +24,31 @@ $(document).ready(function () {
                 data: formData,
                 success : function (data) {
                     $('#create-category')[0].reset();
-                    console.log(data);
+                    jQuery.getMessageSuccess(data.message);
+                    $('#add').modal('hide');
+                    setTimeout(function () {
+                        location.reload();
+                    }, 3000);
+                },
+                error : function (data) {
+                    let error = $.parseJSON(data.responseText);
+
+                    if(data.status === 422) {
+                        if(error.errors.category_name != null) {
+                            $('.error-category-name').text(error.errors.category_name);
+                            $('.error-category-name').removeClass('hidden');
+                        } else {
+                            $('.error-category-name').text('');
+                            $('.error-category-name').addClass('hidden');
+                        }
+                        if(error.errors.category_order != '') {
+                            $('.error-category-order').text(error.errors.category_order);
+                            $('.error-category-order').removeClass('hidden');
+                        } else {
+                            $('.error-category-order').text('');
+                            $('.error-category-order').addClass('hidden');
+                        }
+                    }
                 }
             });
         })
