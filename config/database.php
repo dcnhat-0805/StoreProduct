da810baf710b6612f08923d0c0640df7571498af
nhat.dc <nhat.dc@deha-soft.com>
2019-11-03 18:32:57 +0700

    first deploy

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 return [
 
+    $DATABASE_URL=parse_url('postgres://hyuqkdcvnnmyic:45b954fb4dc178b21765cc1e59300981bdec8b6ad5588b8706e653bd5f3e966e@ec2-54-243-44-102.compute-1.amazonaws.com:5432/d1i2df1116bljg'),
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -13,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,15 +57,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
