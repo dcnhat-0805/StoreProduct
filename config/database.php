ed20d9008e6d8ef37b7da527271a8287f7a19611
nhat.dc <nhat.dc@deha-soft.com>
2019-11-04 18:33:24 +0700

    update database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'ec2-23-21-87-183.compute-1.amazonaws.com'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd1539ql26fps4t'),
-            'username' => env('DB_USERNAME', 'gwwbvlbxufzkna'),
-            'password' => env('DB_PASSWORD', '4af59b20842f43f02803b36794f78e95941ac07cd3498bbc5c2407502f3dcf8e'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'ec2-23-21-87-183.compute-1.amazonaws.com'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd1539ql26fps4t'),
-            'username' => env('DB_USERNAME', 'gwwbvlbxufzkna'),
-            'password' => env('DB_PASSWORD', '4af59b20842f43f02803b36794f78e95941ac07cd3498bbc5c2407502f3dcf8e'),
+            'host' => env('DB_HOST', 'host'),
+            'port' => env('DB_PORT', 'port'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
