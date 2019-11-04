634db17a0f6bfc616d2ad6273f2cfa4cafc1a87d
nhat.dc <nhat.dc@deha-soft.com>
2019-11-04 22:38:12 +0700

    fix reload

--- public/backend/js/backend/product_category.js
+++ public/backend/js/backend/product_category.js
@@ -20,7 +20,7 @@ let productCategoryJs = (function ($) {
             data: data,
             success : function (data) {
                 btnAddProductCategory.prop('disabled', true);
-                window.window.location.reload();
+                window.location.reload(true)
             },
             error : function (data) {
                 let error = (typeof data['responseJSON'] !== 'undefined') ? data['responseJSON'].errors : [];
@@ -60,7 +60,7 @@ let productCategoryJs = (function ($) {
             data: data,
             success : function (data) {
                 btnUpdateProductCategory.prop('disabled', true);
-                window.location.reload();
+                window.location.reload(true)
             },
             error : function (data) {
                 let error = (typeof data['responseJSON'] !== 'undefined') ? data['responseJSON'].errors : [];
@@ -89,10 +89,10 @@ let productCategoryJs = (function ($) {
             },
             success : function (data) {
                 btnDeleteProductCategory.prop('disabled', true);
-                window.location.reload();
+                window.location.reload(true)
             },
             error : function (data) {
-                window.location.reload();
+                window.location.reload(true)
             }
         });
     };
@@ -184,10 +184,10 @@ let productCategoryJs = (function ($) {
             success : function (data) {
                 Commons.removeLocalStorage(PRODUCT_CATEGORY_IDS);
                 Commons.removeLocalStorage(PRODUCT_CATEGORY_DELETE_ALL);
-                window.location.reload();
+                window.location.reload(true)
             },
             error : function (data) {
-                window.location.reload();
+                window.location.reload(true)
             }
         });
     };
